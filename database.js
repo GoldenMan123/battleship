62962a2f3b0adb4c528065d86ef04cbf1a2c4bfa
Vladimir Zhukov <zhvv117@gmail.com>
2016-05-01 18:13:51 +0300

    separated database module

---
+++ database.js
@@ -0,0 +1,65 @@
+var battleship = require('./battleship.js')
+
+
+function create_user(uid) {
+    var user = {}
+    user.uid = uid
+    return user
+}
+
+function get_user_from_db(db, uid, callback, create) {
+    var users = db.collection('users')
+    users.findOne({uid: uid}, function (err, doc) {
+        if (doc) {
+            callback(doc)
+        } else {
+            if (create) {
+                users.insert(create_user(uid), {w:1}, function(err, response) {
+                    if (!err) {
+                        get_user_from_db(db, uid, callback)
+                    }
+                })
+            }
+        }
+    })
+}
+
+function get_game_from_db(db, uid1, uid2, callback, create) {
+    var games = db.collection("game")
+    var archive = db.collection("archive")
+    games.findOne({uid1: uid1, uid2: uid2}, function (err, doc) {
+        if (doc) {
+            if (create) {
+                archive.insert(doc, {w:1}, function (err, response) {
+                    if (!err) {
+                        games.deleteOne(doc, {w:1}, function (err, response) {
+                            game = battleship.newGame()
+                            game.uid1 = uid1
+                            game.uid2 = uid2
+                            games.insert(game, {w:1}, function (err, response) {
+                                if (!err) {
+                                    get_game_from_db(db, uid1, uid2, callback, false)
+                                }
+                            })
+                        })    
+                    }
+                })
+            } else {
+                callback(doc)
+            }
+        } else {
+            game = battleship.newGame()
+            game.uid1 = uid1
+            game.uid2 = uid2
+            games.insert(game, {w:1}, function (err, response) {
+                if (!err) {
+                    get_game_from_db(db, uid1, uid2, callback, false)
+                }
+            })
+        }
+    })
+}
+
+module.exports.create_user = create_user
+module.exports.get_user_from_db = get_user_from_db
+module.exports.get_game_from_db = get_game_from_db
--- main.js
+++ main.js
@@ -8,6 +8,11 @@ var querystring = require('querystring')
 var battleship = require('./battleship.js')
 var ws = require('ws')
 var https = require('https')
+var database = require('./database.js')
+
+var create_user = database.create_user
+var get_user_from_db = database.get_user_from_db
+var get_game_from_db = database.get_game_from_db
 
 
 /// HTTP SERVER PART
@@ -108,66 +113,7 @@ function broadcast_users(user_table, user_info) {
 }
 
 
-/// DATABASE PART
-
-function create_user(uid) {
-    var user = {}
-    user.uid = uid
-    return user
-}
-
-function get_user_from_db(db, uid, callback, create) {
-    var users = db.collection('users')
-    users.findOne({uid: uid}, function (err, doc) {
-        if (doc) {
-            callback(doc)
-        } else {
-            if (create) {
-                users.insert(create_user(uid), {w:1}, function(err, response) {
-                    if (!err) {
-                        get_user_from_db(db, uid, callback)
-                    }
-                })
-            }
-        }
-    })
-}
-
-function get_game_from_db(db, uid1, uid2, callback, create) {
-    var games = db.collection("game")
-    var archive = db.collection("archive")
-    games.findOne({uid1: uid1, uid2: uid2}, function (err, doc) {
-        if (doc) {
-            if (create) {
-                archive.insert(doc, {w:1}, function (err, response) {
-                    if (!err) {
-                        games.deleteOne(doc, {w:1}, function (err, response) {
-                            game = battleship.newGame()
-                            game.uid1 = uid1
-                            game.uid2 = uid2
-                            games.insert(game, {w:1}, function (err, response) {
-                                if (!err) {
-                                    get_game_from_db(db, uid1, uid2, callback, false)
-                                }
-                            })
-                        })    
-                    }
-                })
-            } else {
-                callback(doc)
-            }
-        } else {
-            game = battleship.newGame()
-            game.uid1 = uid1
-            game.uid2 = uid2
-            games.insert(game, {w:1}, function (err, response) {
-                if (!err) {
-                    get_game_from_db(db, uid1, uid2, callback, false)
-                }
-            })
-        }
-    })
-}
+/// START SERVER
 
 mongo.connect("mongodb://localhost:27017/battleship", function (err, db) {
     if (err) {
