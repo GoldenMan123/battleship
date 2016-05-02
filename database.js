2941b83cafe6af6b1767a4f5e1f7b0f3f5bc9ab3
Vladimir Zhukov <zhvv117@gmail.com>
2016-05-03 00:52:58 +0300

    database: promisification

--- database.js
+++ database.js
@@ -9,54 +9,83 @@ function create_user(uid) {
 
 function get_user_from_db(db, uid, callback, create) {
     var users = db.collection('users')
-    users.findOne({uid: uid}, function (err, doc) {
-        if (doc) {
-            callback(doc)
-        } else {
-            if (create) {
-                users.insert(create_user(uid), {w:1}, function(err, response) {
-                    if (!err) {
-                        get_user_from_db(db, uid, callback)
-                    }
-                })
+
+    var getUser = new Promise(function(resolve, reject) {
+        users.findOne({uid: uid}, function(err, doc) {
+            if (!doc) {
+                reject(err)
+            } else {
+                resolve(doc)
             }
-        }
+        })
+    })
+
+    getUser.then(doc => callback(doc), () => {
+        users.insert(create_user(uid), {w:1}, function(err, _) {
+            if (!err) {
+                get_user_from_db(db, uid, callback, false)
+            }
+        })
     })
 }
 
 function get_game_from_db(db, uid1, uid2, callback, create) {
     var games = db.collection("game")
     var archive = db.collection("archive")
-    games.findOne({uid1: uid1, uid2: uid2}, function (err, doc) {
-        if (doc) {
-            if (create) {
+
+    findGame = new Promise(function (resolve, reject) {
+        games.findOne({uid1: uid1, uid2: uid2}, function (err, doc) {
+            if (!doc) {
+                reject(err)
+            } else { 
+                resolve(doc)
+            }
+        })
+    })
+
+    findGame.then(doc => {
+        if (create) {
+            archiveInsert = new Promise(function (resolve, reject) {
                 archive.insert(doc, {w:1}, function (err, response) {
-                    if (!err) {
-                        games.deleteOne(doc, {w:1}, function (err, response) {
-                            game = battleship.newGame()
-                            game.uid1 = uid1
-                            game.uid2 = uid2
-                            games.insert(game, {w:1}, function (err, response) {
-                                if (!err) {
-                                    get_game_from_db(db, uid1, uid2, callback, false)
-                                }
-                            })
-                        })    
+                    if (err) {
+                        reject(err)
+                    } else {
+                        resolve(doc)
                     }
                 })
-            } else {
-                callback(doc)
-            }
-        } else {
-            game = battleship.newGame()
-            game.uid1 = uid1
-            game.uid2 = uid2
-            games.insert(game, {w:1}, function (err, response) {
-                if (!err) {
-                    get_game_from_db(db, uid1, uid2, callback, false)
-                }
             })
+            archiveInsert.then(doc => {
+                return new Promise(function (resolve, reject) {
+                    games.deleteOne(doc, {w:1}, function (err, doc) {
+                        if (err) {
+                            reject(err)
+                        } else {
+                            resolve(doc)
+                        }
+                    })
+                })
+            }).then(doc => {
+                game = battleship.newGame()
+                game.uid1 = uid1
+                game.uid2 = uid2
+                games.insert(game, {w:1}, function (err, response) {
+                    if (!err) {
+                        get_game_from_db(db, uid1, uid2, callback, false)
+                    }
+                })
+            })
+        } else {
+            callback(doc)
         }
+    }, () => {
+        game = battleship.newGame()
+        game.uid1 = uid1
+        game.uid2 = uid2
+        games.insert(game, {w:1}, function (err, response) {
+            if (!err) {
+                get_game_from_db(db, uid1, uid2, callback, false)
+            }
+        })
     })
 }
 
